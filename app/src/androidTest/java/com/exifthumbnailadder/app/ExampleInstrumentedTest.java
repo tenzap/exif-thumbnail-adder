2b9af47f6d9efbc6ab1b60babbd42cc741fa58ff
tenzap <fabstz-it@yahoo.fr>
2021-03-31 13:26:06 +0200

    add fastlane config and screenshot automation + changelog

--- app/src/androidTest/java/com/exifthumbnailadder/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/ExampleInstrumentedTest.java
@@ -21,13 +21,32 @@ package com.exifthumbnailadder.app;
 
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -36,10 +55,47 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void beforeAll() {
+        //Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.exifthumbnailadder", appContext.getPackageName());
+        assertEquals("com.exifthumbnailadder.app", appContext.getPackageName());
+    }
+
+    @Test
+    public void testTakeScreenshot() {
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+
+        Screengrab.screenshot("01-mainActivityClean");
+
+        // Your custom onView...
+        onView(withId(R.id.button_settings)).perform(click());
+        Screengrab.screenshot("02-settings-1");
+
+        onView(withId(R.id.settings)).perform(swipeUp());
+        Screengrab.screenshot("02-settings-2");
+
+        onView(withId(R.id.settings)).perform(pressBack());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText(R.string.action_about)).perform(click());
+        Screengrab.screenshot("03-about");
+
+        Espresso.pressBack();
+        onView(withId(R.id.button_addThumbs)).perform(click());
+        Screengrab.screenshot("04-finishedProcessing");
+
+        // TODO : screenshot with a configured app & done when "Start Processing" is finished
+
     }
 }
