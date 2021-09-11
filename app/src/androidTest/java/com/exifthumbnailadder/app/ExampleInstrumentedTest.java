96a647204d13cb87211ba8769a3ee57e1d49daa7
tenzap <fabstz-it@yahoo.fr>
2021-09-11 13:17:19 +0200

    update screenshot process to honour debug appId

--- app/src/androidTest/java/com/exifthumbnailadder/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/ExampleInstrumentedTest.java
@@ -84,7 +84,11 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.exifthumbnailadder.app", appContext.getPackageName());
+        String appIdSuffix = "";
+        if (BuildConfig.BUILD_TYPE.equals("debug")) {
+            appIdSuffix = ".debug";
+        }
+        assertEquals("com.exifthumbnailadder.app" + appIdSuffix, appContext.getPackageName());
     }
 
     static {
