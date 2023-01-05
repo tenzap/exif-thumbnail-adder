5869f79774f24b9ab93d3a7865b5dfce46388038
tenzap <fabstz-it@yahoo.fr>
2023-01-05 17:32:01 +0100

    TestUtil: update click on ShowInternalStorage

--- app/src/androidTest/java/com/exifthumbnailadder/app/TestUtil.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/TestUtil.java
@@ -92,8 +92,8 @@ public class TestUtil {
 
         // Click on "Show internal storage" if it is there, otherwise press back to quit the "More options" menu
         UiObject showInternalStorage = device.findObject(new UiSelector().text(docUIStrings.getShowInternalStorage()));
-        try { showInternalStorage.clickAndWaitForNewWindow(); }
-        catch (UiObjectNotFoundException e) { device.pressBack(); }
+        if (showInternalStorage.exists()) { showInternalStorage.clickAndWaitForNewWindow(); }
+        else { device.pressBack(); }
 
         // Open Drawer
         UiObject hamburgerMenu = device.findObject(new UiSelector().clickable(true).description(docUIStrings.getShowRoots()));
