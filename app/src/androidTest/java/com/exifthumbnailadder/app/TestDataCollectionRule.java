f551624ed99086ae4d34eddf55a383568e30480c
tenzap <fabstz-it@yahoo.fr>
2023-02-07 22:06:43 +0100

    Tests: record also logcat for each test

--- app/src/androidTest/java/com/exifthumbnailadder/app/TestDataCollectionRule.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/TestDataCollectionRule.java
@@ -20,13 +20,11 @@
 
 package com.exifthumbnailadder.app;
 
-import androidx.annotation.WorkerThread;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.uiautomator.UiDevice;
+import android.util.Log;
 
-import java.io.IOException;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
@@ -34,42 +32,46 @@ import org.junit.runner.Description;
 // Inspired from https://medium.com/jumio/how-to-record-screen-captures-during-ui-tests-on-android-67ba5dedb7e1
 public class TestDataCollectionRule extends TestWatcher {
 
-    private final UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    private final String ScPath = "/data/local/tmp/screenrecords/" + android.os.Build.VERSION.SDK_INT;
+    private Date testStartTime;
 
-    @WorkerThread
-    public void startScreenRecord(String fileName) throws IOException {
-//        try {
-        uiDevice.executeShellCommand("mkdir -p " + ScPath);
-        uiDevice.executeShellCommand("screenrecord --bit-rate 5M --bugreport " + ScPath + "/" + fileName + ".mp4");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+    private void setTestStartDate() {
+        if (testStartTime == null) {
+            testStartTime = Calendar.getInstance().getTime();
+        }
     }
 
-    @WorkerThread
-    public void stopScreenRecord() throws IOException {
-//        try {
-        uiDevice.executeShellCommand("pkill -2 screenrecord");
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
+    private Date getTestStartDate() {
+        return testStartTime;
+    }
+
+    private String formatDateForTestName(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        return simpleDateFormat.format(date);
+    }
+
+    private String formatDateForLogcat(Date date) {
+        // We can't use the date format with a space because executeShellCommand
+        // doesn't support it.
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss.SSS");
+        return simpleDateFormat.format(date);
     }
 
     private String getTestName(Description description) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        String timeStamp = simpleDateFormat.format(Calendar.getInstance().getTime());
+        String timeStamp = formatDateForTestName(getTestStartDate());
         return timeStamp + "_" + BuildConfig.FLAVOR + "_" + description.getMethodName();
     }
 
     @Override
     protected void starting(Description description) {
         super.starting(description);
+        setTestStartDate();
+        // Start Screenrecord in a thread, because executeShellCommand
+        // waits until the command is finished ans screerecord is a blocking command
         new Thread() {
             public void run() {
                 super.run();
-                try { startScreenRecord(getTestName(description)); }
-                catch (Exception e) { e.printStackTrace(); }
+                Log.d("ETATest", "ScreenRecord: starting");
+                TestDataCollectionUtils.startScreenRecord(getTestName(description));
             }
         }.start();
     }
@@ -77,13 +79,11 @@ public class TestDataCollectionRule extends TestWatcher {
     @Override
     protected void finished(Description description) {
         super.finished(description);
-        new Thread() {
-            public void run() {
-                super.run();
-                try { stopScreenRecord(); }
-                catch (Exception e) { e.printStackTrace(); }
-            }
-        }.start();
-    }
 
+        Log.d("ETATest", "ScreenRecord: stopping");
+        TestDataCollectionUtils.stopScreenRecord();
+
+        Log.d("ETATest", "save Logcat");
+        TestDataCollectionUtils.storeLogcat(getTestName(description), formatDateForLogcat(getTestStartDate()));
+    }
 }
---
+++ app/src/androidTest/java/com/exifthumbnailadder/app/TestDataCollectionUtils.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2023 Fab Stz <fabstz-it@yahoo.fr>
+ *
+ * This file is part of Exif Thumbnail Adder. An android app that adds
+ * thumbnails in EXIF tags of your pictures that don't have one yet.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.exifthumbnailadder.app;
+
+import android.util.Log;
+
+import androidx.annotation.WorkerThread;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+@RunWith(AndroidJUnit4.class)
+public class TestDataCollectionUtils {
+
+    @Rule
+    public static TestDataCollectionRule testDataCollectionRule = new TestDataCollectionRule();
+
+    protected static final UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    private static final String ScPath = "/data/local/tmp/screenrecords/" + android.os.Build.VERSION.SDK_INT;
+
+    @WorkerThread
+    public static void startScreenRecord(String fileName) {
+        try {
+            uiDevice.executeShellCommand("mkdir -p " + ScPath);
+            uiDevice.executeShellCommand("screenrecord --bit-rate 5M --bugreport " + ScPath + "/" + fileName + ".mp4");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @WorkerThread
+    public static void stopScreenRecord() {
+        try {
+            uiDevice.executeShellCommand("pkill -2 screenrecord");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @WorkerThread
+    public static void storeLogcat(String fileName, String date) {
+        String logcatOutputFilename = ScPath + "/" + fileName + ".logcat.txt";
+        String logcatCommand = "logcat -t " + date + " -f " + logcatOutputFilename;
+        Log.d("ETATest", "logcatCommand: " + logcatCommand);
+
+        try {
+            String output = uiDevice.executeShellCommand(logcatCommand);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
