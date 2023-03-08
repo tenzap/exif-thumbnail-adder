75e1e0cab01be2b4226b734ea84361c57e534b88
tenzap <fabstz-it@yahoo.fr>
2023-03-08 19:57:58 +0100

    fix license header

--- app/src/main/cpp/tests.c
+++ app/src/main/cpp/tests.c
@@ -1,6 +1,22 @@
-//
-// Created by fabien on 02/02/2023.
-//
+/*
+ * Copyright (C) 2023 Fab Stz <fabstz-it@yahoo.fr>
+ *
+ * This file is part of Exif Thumbnail Adder. An android app that adds
+ * thumbnails in EXIF tags of your pictures that don't have one yet.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <jni.h>
 
