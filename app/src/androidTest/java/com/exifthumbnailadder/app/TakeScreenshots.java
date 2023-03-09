d65395967560f79d77f5c2c6ea3c2197e748c9ff
tenzap <fabstz-it@yahoo.fr>
2023-03-09 15:40:56 +0100

    update screenshot procedure to fit current state of the app

--- app/src/androidTest/java/com/exifthumbnailadder/app/TakeScreenshots.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/TakeScreenshots.java
@@ -20,12 +20,21 @@
 
 package com.exifthumbnailadder.app;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.util.Log;
 
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.PerformException;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -42,6 +51,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import static org.hamcrest.core.AllOf.allOf;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -66,11 +78,15 @@ import tools.fastlane.screengrab.locale.LocaleTestRule;
  */
 @RunWith(AndroidJUnit4.class)
 public class TakeScreenshots {
+    private IdlingResource mIdlingResource;
+    private IdlingResource mWorkingDirPermIdlingResource;
+    public boolean finished;
+
     @ClassRule
     public static final LocaleTestRule localeTestRule = new LocaleTestRule();
 
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @BeforeClass
     public static void beforeAll() {
@@ -157,9 +173,7 @@ public class TakeScreenshots {
         editor.commit();
 
         // give all files access (we need it to delete folders)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !PermissionManager.hasAllFilesAccessPermission()) {
-            TestUtil.requestAllFilesAccess();
-        }
+        TestUtil.requestAllFilesAccess();
 
         // Delete existing WorkingDir (so that we can go to the "WorkingDirPermActivity")
         deleteDirectory(Paths.get("/storage/emulated/0/ThumbAdder-sg").toFile());
@@ -167,30 +181,74 @@ public class TakeScreenshots {
         // Go to "Add thumbnails" fragment
         onView(withId(R.id.AddThumbsFragment)).perform(click());
 
+        finished = false;
+        // Register BroadcastReceiver of the signal saying that processing is finished
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case "com.exifthumbnailadder.app.ADD_THUMBS_FRAGMENT_FINISHED":
+                        finished = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.exifthumbnailadder.app.ADD_THUMBS_FRAGMENT_FINISHED");
+        LocalBroadcastManager.getInstance(context)
+                .registerReceiver(receiver, filter);
+
         // Run "Add thumbnails" which brings us to the WorkingDirPermActivity
         onView(withId(R.id.button_addThumbs)).perform(click());
 
-//        // Give WRITE_EXTERNAL_STORAGE permissions for <R & not 'standard')
-//        // Doesn't seem necessary as permission is already given in this test case
-//        if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && BuildConfig.FLAVOR.equals("standard"))) {
-//            onView(withText(context.getString(android.R.string.ok))).perform(click());
-//
-//            uiElement = device.findObject(new UiSelector().clickable(true).textContains(allow));
-//            try { uiElement.clickAndWaitForNewWindow(); }
-//            catch (Exception e) { e.printStackTrace(); }
-//        }
+        for (String perm : PermissionManager.getRequiredPermissions(prefs)) {
+            if (!PermissionManager.isPermissionGranted(context, perm)) {
+                if (perm.equals(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE))
+                    continue;
+                if (perm.equals(android.Manifest.permission.ACCESS_MEDIA_LOCATION)) {
+                    if (PermissionManager.isPermissionGranted(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                            PermissionManager.isPermissionGranted(context, android.Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                            PermissionManager.isPermissionGranted(context, Manifest.permission.READ_MEDIA_IMAGES))
+                        continue;
+                }
+                TestUtil.clickPermissionAllowButton();
+            }
+        }
 
         Screengrab.screenshot(String.format("%03d", ++i));
 
         // Give permissions to the WorkingDir
-        onView(withId(R.id.button_checkPermissions)).perform(click());
+        IdlingRegistry.getInstance().register(mWorkingDirPermIdlingResource);
+        TestUtil.workingDirPermActivityCheckPermission();
+        IdlingRegistry.getInstance().unregister(mWorkingDirPermIdlingResource);
+
+        Log.d("ETATest", "Before givePermissionToWorkingDir()");
         TestUtil.givePermissionToWorkingDir();
+        Log.d("ETATest", "After givePermissionToWorkingDir()");
 
-        // Restart processing now that permissions to WorkingDir are given
-        onView(withId(R.id.button_addThumbs)).perform(click());
 
-        // Wait 5 sec before taking screenshot
-        Thread.sleep(5000);
+        // Wait until processing is finished or has hit timeout (duration is in ms)
+        long max_duration = 20 * 60 * 1000;
+        long timeout = System.currentTimeMillis() + max_duration;
+        while (!finished && System.currentTimeMillis() < timeout) {
+            Thread.sleep(1000);
+        }
+
+        // Stop processing if not finished (ie. in the case timeout was hit)
+        if (!finished) {
+            try {
+                onView(withId(R.id.button_stopProcess)).perform(click());
+            } catch (PerformException e) {
+                // This exception happens when button_stopProcess is not in the view.
+                e.printStackTrace();
+            }
+        }
+
+        // Unregister BroadcastReceiver
+        LocalBroadcastManager.getInstance(context)
+                .unregisterReceiver(receiver);
 
         Screengrab.screenshot(String.format("%03d", ++i));
 
@@ -208,4 +266,21 @@ public class TakeScreenshots {
         return directoryToBeDeleted.delete();
     }
 
+    @Before
+    public void registerIdlingResource() {
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            mIdlingResource = MainActivity.getIdlingResource();
+            // To prove that the test fails, omit this call:
+            IdlingRegistry.getInstance().register(mIdlingResource);
+
+            mWorkingDirPermIdlingResource = MainActivity.getWorkingDirPermIdlingResource();
+        });
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
+    }
 }
