b421a2577ea4e5d584526d22b15ac6cff491d424
tenzap <fabstz-it@yahoo.fr>
2023-03-17 10:09:35 +0100

    Tests: restart screenrecord every 160sec
    Otherwise, it would just record the first 3 minutes

--- app/src/androidTest/java/com/exifthumbnailadder/app/TestDataCollectionRule.java
+++ app/src/androidTest/java/com/exifthumbnailadder/app/TestDataCollectionRule.java
@@ -24,13 +24,24 @@ import android.util.Log;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
 // Inspired from https://medium.com/jumio/how-to-record-screen-captures-during-ui-tests-on-android-67ba5dedb7e1
 public class TestDataCollectionRule extends TestWatcher {
+    Timer timer = new Timer();
 
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            restartScreenRecord();
+        }
+    };
+
+    Description description;
     private LocalDateTime testStartTime;
 
     private void setTestStartDate() {
@@ -61,12 +72,18 @@ public class TestDataCollectionRule extends TestWatcher {
         return timeStamp + "_" + BuildConfig.FLAVOR + "_" + description.getMethodName();
     }
 
+    private String getTestName(Description description, LocalDateTime date) {
+        String timeStamp = formatDateForTestName(date);
+        return timeStamp + "_" + BuildConfig.FLAVOR + "_" + description.getMethodName();
+    }
+
     @Override
     protected void starting(Description description) {
+        this.description = description;
         super.starting(description);
         setTestStartDate();
         // Start Screenrecord in a thread, because executeShellCommand
-        // waits until the command is finished ans screerecord is a blocking command
+        // waits until the command is finished and screerecord is a blocking command
         new Thread() {
             public void run() {
                 super.run();
@@ -74,6 +91,7 @@ public class TestDataCollectionRule extends TestWatcher {
                 TestDataCollectionUtils.startScreenRecord(getTestName(description));
             }
         }.start();
+        timer.schedule(timerTask, 160*1000, 160*1000);
     }
 
     @Override
@@ -85,5 +103,19 @@ public class TestDataCollectionRule extends TestWatcher {
 
         Log.d("ETATest", "save Logcat");
         TestDataCollectionUtils.storeLogcat(getTestName(description), formatDateForLogcat(getTestStartDate()));
+
+        timer.cancel();
+    }
+
+    private void restartScreenRecord() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                Log.d("ETATest", "ScreenRecord: restarting");
+                TestDataCollectionUtils.stopScreenRecord();
+                TestDataCollectionUtils.startScreenRecord(getTestName(description, LocalDateTime.now()));
+            }
+        }.start();
     }
 }
